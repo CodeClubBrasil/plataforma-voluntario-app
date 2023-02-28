aaaecdec3abd733113dbeae3d8c90db081c50e22
pedrocerqueiras <pedrosilvap2@hotmailcom>
2023-02-28 19:00:40 -0300

    inicial commit

---
+++ app/src/main/java/com/example/codeclubapp/Login.kt
@@ -0,0 +1,18 @@
+package com.example.codeclubapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val fazercadastro: Button = findViewById(R.id.botaoFazerCadastro)
+        fazercadastro.setOnClickListener { val intent = Intent (this, Cadastro::class.java)
+        startActivity(intent)}
+
+    }
+}
