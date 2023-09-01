fab041f29ea4b7a25c32ba99ba49cd7ef8671c87
William <william.progdata@gmail.com>
2023-09-01 16:34:02 -0300

    Salvando usuários no db local

--- app/src/main/java/com/example/codeclubapp/SignUp01.kt
+++ app/src/main/java/com/example/codeclubapp/SignUp01.kt
@@ -3,6 +3,7 @@ package com.example.codeclubapp
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
@@ -16,6 +17,7 @@ import com.example.codeclubapp.src.classesModelos.user.DAYS
 import com.example.codeclubapp.src.classesModelos.user.Knowledges
 import com.example.codeclubapp.src.classesModelos.user.UserCodeClub
 import com.example.codeclubapp.src.ui.viewmodel.SignUpViewModel
+import com.example.codeclubapp.src.utils.CC_Utils
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignUp01 : AppCompatActivity() {
@@ -39,36 +41,57 @@ class SignUp01 : AppCompatActivity() {
 
 
         binding.btnContinueToSignUp02.setOnClickListener {
-            saveUserOnDatabase()
+            val name = binding.editTextNome.text.toString()
+            val lastName = binding.editTextSobrenome.text.toString()
+            val password = binding.editTextSenha.text.toString()
+            val city = binding.editTextCidade.text.toString()
+            val neighborhood = binding.editTextBairro.text.toString()
+            val state = "buscar estado do Spinner"
+            val tels = binding.editTextTelefone.text.toString()
+            val email = binding.editTextEmail.text.toString()
+            val knowledges = binding.editTextKnowledge.text.toString()
+
+            Log.i(TAG, " Salvando usuario de nome: $name $lastName e city: $city ")
+            CC_Utils.showToast(this,"Usuario $name cadastrado com sucesso")
+            saveUserOnDatabase(name,lastName,password,city,state,neighborhood,tels,email, knowledges)
         }
 
         val entrar: Button = findViewById(R.id.botaoEntrar)
         entrar.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, Login::class.java))
         }
 
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun saveUserOnDatabase() {
-        val userAddress = Address("castanhal", "PA", "santa lidia")
-        val userContacts = Contacts("9196765641", "emailx@gmail.com")
+    private fun saveUserOnDatabase(
+        name: String,
+        lastName: String,
+        password: String,
+        city: String,
+        state: String,
+        neighborhood: String,
+        tels:String,
+        email:String,
+        knowledges: String,
+    ) {
+        val userAddress = Address(city, state, neighborhood)
+        val userContacts = Contacts(tels, email)
         val avaiableTime = AvaiableTime(
             DAYS.MONDAY,
             System.currentTimeMillis(),
             System.currentTimeMillis()
         )
-        val knowLedges = Knowledges("KOTLIN")
+        //val knowLedges = Knowledges("KOTLIN")
         val user = UserCodeClub(
             0,
-            "VALTER",
-            "GABRIEL",
-            "1234",
+            name,
+            lastName,
+            password,
             userAddress,
             userContacts,
             avaiableTime,
-            knowLedges
+            Knowledges(knowledges)
         )
         signUpViewModel.insertNewUser(user)
     }
